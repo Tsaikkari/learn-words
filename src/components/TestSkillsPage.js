5aa91797c00c64005660fb0814569ec8f0f318c9
Tsaikkari <trospe@gmx.de>
2020-06-20 11:55:26 +0200

    Styling

--- src/components/TestSkillsPage.js
+++ src/components/TestSkillsPage.js
@@ -43,7 +43,7 @@ export class TestSkillsPage extends React.Component {
         >
           Pick Word
         </button>
-        <h3>{this.state.pickedWord}</h3>
+        <h3 class="picked-word">{this.state.pickedWord}</h3>
         <input
           type="text"
           className="text-input text-input__answer"
@@ -56,7 +56,7 @@ export class TestSkillsPage extends React.Component {
           className="button button__submit" 
           onClick={this.onSubmit}
         >
-          Submit Answer
+          Submit
         </button>
       </div>
     );
