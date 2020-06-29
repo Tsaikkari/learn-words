972e12b7cd069474b4e4f78973621e8ab0cf0165
Tsaikkari <trospe@gmx.de>
2020-06-29 10:29:25 +0200

    Import startIncrementCount

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetWords } from './actions/words';
+import { startIncrementCount } from './actions/count';
 import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -32,7 +33,7 @@ ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(login(user.uid));
-    store.dispatch(startSetWords()).then(() => {
+    store.dispatch(startSetWords(), startIncrementCount()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
         history.push('/dashboard')
